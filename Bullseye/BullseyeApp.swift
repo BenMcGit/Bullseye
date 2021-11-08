e4a6e2e9a492151c3f7b42b804d242995e18cddb
Benjamin McAdams <bmcadams@twitter.com>
2021-11-08 12:50:52 -0800

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Benjamin McAdams on 11/8/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
